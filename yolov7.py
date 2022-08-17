d626feffce6eb37ca535b415a6b28a780c3d8710
unknown <jachang3456@gmail.com>
2022-08-17 09:32:12 +0900

    commit

--- yolov7.py
+++ yolov7.py
@@ -5,8 +5,7 @@ image = (".jpg", ".png", ".jpeg")
 video = (".mp4", ".avi", ".wmv")
 
 def run_Yolo() :
-    os.system("ls -al")
-    os.system("df -h")
+    os.system("history")
     st.subheader('yolov7')
     uploaded_file = st.file_uploader("Inference할 이미지나, 동영상 파일을 선택해주세요.")
     st.text("서버의 용량 문제로, 여러개의 파일은 지원하지 않습니다.")
